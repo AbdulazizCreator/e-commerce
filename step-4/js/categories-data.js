3592cc1d83114a863d56d0a32ad0c843b07eef5a
abdulazizcreator <abdulazizprogrammer@gmail.com>
2023-06-01 22:46:53 +0500

    step-4 finished

---
+++ step-4/js/categories-data.js
@@ -0,0 +1,32 @@
+const categories = [
+  { id: 1, name: "Fruit", image: "https://picsum.photos/200/300/?category0" },
+  { id: 2, name: "Bakery", image: "https://picsum.photos/200/300/?category2" },
+  {
+    id: 3,
+    name: "Vegetable",
+    image: "https://picsum.photos/200/300/?category3",
+  },
+  { id: 4, name: "Dairy", image: "https://picsum.photos/200/300/?category5" },
+  { id: 5, name: "Meat", image: "https://picsum.photos/200/300/?category6" },
+  {
+    id: 6,
+    name: "Dessert",
+    image: "https://picsum.photos/200/300/?category7",
+  },
+  {
+    id: 7,
+    name: "Fast food",
+    image: "https://picsum.photos/200/300/?category12",
+  },
+  {
+    id: 8,
+    name: "Breakfast",
+    image: "https://picsum.photos/200/300/?category23",
+  },
+  { id: 9, name: "Lunch", image: "https://picsum.photos/200/300/?category27" },
+  {
+    id: 10,
+    name: "Dinner",
+    image: "https://picsum.photos/200/300/?category31",
+  },
+];
